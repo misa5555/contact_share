b988123760473b4accef536a729c6a277f5e79e1
App Academy Student <student@appacademy.io>
2014-08-25 16:27:26 -0700

    add contact_shares model and migration

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,20 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true, uniqueness: {scope: :email}
   validates :email, presence: true, uniqueness: {scope: :name}
 
-  validates :user_id, presnece: true, uniqueness: true
-  belongs_to :user
+  validates :user_id, presence: true, uniqueness: true
+
+
+  belongs_to (
+    :owner,
+    class_name: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+
+  has_many :contact_shares
+  has_many(
+    :shared_users,
+    through: :contact_shares,
+    source: :user
+  )
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  has_many(
+    :shared_contacts,
+    through: :contact_shares,
+    source: :contact
+  )
   has_many :contacts
+  has_many :contact_shares
 end
